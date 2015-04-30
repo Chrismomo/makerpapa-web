29fc2f63ddaf006b05d871b54c44404636fbf5af
Peter Wang <msnwangping@hotmail.com>
2015-04-30 23:01:22 +0100

    picture slide

--- client/app/controllers/2.community.controller.js
+++ client/app/controllers/2.community.controller.js
@@ -17,5 +17,20 @@ angular.module('makerPaPaApp')
             );
         }
 
-        loadData() 
+        loadData()
+
+        $scope.myInterval = 5000;
+        var slides = $scope.slides = [];
+        $scope.addSlide = function() {
+            var newWidth = 1200 + slides.length + 1;
+            slides.push({
+                image: 'http://placekitten.com/' + newWidth + '/300',
+                text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' +
+                    ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+            });
+        };
+        for (var i = 0; i < 4; i++) {
+            $scope.addSlide();
+        }
+
     });
