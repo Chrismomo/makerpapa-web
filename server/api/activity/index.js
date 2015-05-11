7cbb95843acc3893865403feae222edba14936fd
Peter Wang <msnwangping@hotmail.com>
2015-05-11 22:26:35 +0100

    add mandrill for sending email

--- server/api/activity/index.js
+++ server/api/activity/index.js
@@ -2,10 +2,12 @@
 
 var express = require('express');
 var controller = require('./activity.controller');
+var emailSender = require('../email/email.sender');
 
 var router = express.Router();
 
 router.get('/', controller.index);
+router.get('/email/send',emailSender.send); // http://localhost:9000/api/activitys/email/send
 router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
